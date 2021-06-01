7fc7971744f280c0b05ee3a154607380002231d0
ahadsenja <hamdanilm96@gmail.com>
2021-06-01 13:33:42 +0800

    chore: update app modules

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './components/login/login.component';
+import { MovieListComponent } from './components/movie-list/movie-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    MovieListComponent
   ],
   imports: [
     BrowserModule,
