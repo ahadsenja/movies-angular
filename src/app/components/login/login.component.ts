14836aabf000bdea2aae37428310601a84851506
ahadsenja <hamdanilm96@gmail.com>
2021-06-23 21:11:39 +0800

    feat: create login component

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+
+import { AuthService } from 'src/app/service/auth.service';
+import { TokenStorageService } from 'src/app/service/token-storage.service';
 
 @Component({
   selector: 'app-login',
@@ -7,9 +11,33 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+  form: any = {
+    username: null,
+    password: null
+  }
+  isLoggedIn = false;
+
+  constructor(
+    private auth: AuthService,
+    private token: TokenStorageService,
+    private router: Router
+  ) { }
 
   ngOnInit(): void {
+    console.log(this.form)
+    if(this.token.getToken()) {
+      this.isLoggedIn = true
+    }
   }
 
+  onSubmit(): void {
+    const { username, password } = this.form;
+
+    this.auth.login(username, password).subscribe(data => {
+      this.token.saveToken(data.accessToken);
+      this.token.saveUser(data);
+      this.router.navigate(['/dashboard'])
+      console.log('Login Success')
+    })
+  }
 }
