d8e17fdfd4fa6659b2933f1ff5a03fe89fe9868a
ahadsenja <hamdanilm96@gmail.com>
2021-06-23 21:03:00 +0800

    feat: add Auth Guard to protect route

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,12 +3,14 @@ import { RouterModule, Routes } from '@angular/router';
 
 import { LoginComponent } from './components/login/login.component';
 import { DashboardComponent } from './components/dashboard/dashboard.component';
-import { AuthGuard } from './auth.guard';
+import { MovieListComponent } from './components/movie-list/movie-list.component';
+import { AuthGuard } from './auth/auth.guard';
 
 const routes: Routes = [
-  {path: '', redirectTo: 'login', pathMatch: 'full'},
-  {path: 'login', component: LoginComponent, canActivate: [AuthGuard]},
+  {path: '', redirectTo: 'dashboard', pathMatch: 'full'},
+  {path: 'login', component: LoginComponent},
   {path: 'dashboard', component: DashboardComponent, canActivate: [AuthGuard]},
+  {path: 'movies', component: MovieListComponent, canActivate: [AuthGuard]},
 ];
 
 @NgModule({
