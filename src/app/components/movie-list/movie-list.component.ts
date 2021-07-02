31dcb11619c148368a85c4cf67ba76b9872087cb
ahadsenja <hamdanilm96@gmail.com>
2021-07-02 17:19:44 +0800

    chore: update movie list component

--- src/app/components/movie-list/movie-list.component.ts
+++ src/app/components/movie-list/movie-list.component.ts
@@ -1,9 +1,7 @@
-import { HttpHeaders } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
 
 import { Movie } from 'src/app/models/movie';
 import { MoviesService } from 'src/app/service/movies.service';
-import { TokenStorageService } from 'src/app/service/token-storage.service';
 
 @Component({
   selector: 'app-movie-list',
@@ -14,14 +12,15 @@ export class MovieListComponent implements OnInit {
 
   movies: Movie[] = [];
 
-  constructor(private movieService: MoviesService, private token: TokenStorageService) { }
+  constructor(
+    private movieService: MoviesService, 
+  ) { }
 
   ngOnInit(): void {
     this.onGetMovies();
   }
 
   onGetMovies() {
-    // const headers = new HttpHeaders().set('Authorization', `Token ${this.token.getToken()}`);
     this.movieService.getAll().subscribe(movies => {
       this.movies = movies;
       console.log(movies);
