e5c578619bb7c81489aae90bdac479a725c052bf
ahadsenja <hamdanilm96@gmail.com>
2021-07-05 15:40:01 +0800

    feat: add fontawesome

--- src/app/layout/sidebar/sidebar.component.ts
+++ src/app/layout/sidebar/sidebar.component.ts
@@ -1,11 +1,17 @@
 import { Component, OnInit } from '@angular/core';
 
+import { faTachometerAlt, faDatabase, faSignOutAlt } from '@fortawesome/free-solid-svg-icons';
+
 @Component({
   selector: 'app-sidebar',
   templateUrl: './sidebar.component.html',
   styleUrls: ['./sidebar.component.css']
 })
 export class SidebarComponent implements OnInit {
+
+  tachometer = faTachometerAlt
+  database = faDatabase
+  signOut = faSignOutAlt
 
   constructor() { }
 
