850877dea10fd58d710795fe3c5099ac297eaba5
ahadsenja <hamdanilm96@gmail.com>
2021-07-05 15:34:54 +0800

    feat: add movies create route

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,7 +2,8 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
 import { AuthGuard } from './auth/auth.guard';
-import { LoginComponent } from './components/login/login.component';
+import { LoginComponent } from './components/user/login/login.component';
+import { RegisterComponent } from './components/user/register/register.component';
 import { DashboardComponent } from './components/dashboard/dashboard.component';
 import { MovieListComponent } from './components/movies/movie-list/movie-list.component';
 import { MovieCreateComponent } from './components/movies/movie-create/movie-create.component';
@@ -10,6 +11,7 @@ import { MovieCreateComponent } from './components/movies/movie-create/movie-cre
 const routes: Routes = [
   {path: '', redirectTo: 'dashboard', pathMatch: 'full'},
   {path: 'login', component: LoginComponent},
+  {path: 'register', component: RegisterComponent},
   {path: 'dashboard', component: DashboardComponent, canActivate: [AuthGuard]},
   {path: 'movies', component: MovieListComponent, canActivate: [AuthGuard]},
   {path: 'movies/create', component: MovieCreateComponent, canActivate: [AuthGuard]},
