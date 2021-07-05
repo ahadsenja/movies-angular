821afde1a755588f072378f5a19646e46e721494
ahadsenja <hamdanilm96@gmail.com>
2021-07-05 15:45:22 +0800

    chore: update movie create component

--- src/app/components/movies/movie-create/movie-create.component.ts
+++ src/app/components/movies/movie-create/movie-create.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+
+import { MoviesService } from 'src/app/service/movies.service';
+import { Movie } from 'src/app/models/movie';
 
 @Component({
   selector: 'app-movie-create',
@@ -7,9 +11,47 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MovieCreateComponent implements OnInit {
 
-  constructor() { }
+  movies: Movie[] = [];
+
+  id: number = 0;
+  title: string = '';
+  author: string = '';
+  year: number = 0;
+  genre: string = '';
+  published: boolean = false;
+  image: string = '';
+
+  submitted = false;
+
+  constructor(
+    private movieService: MoviesService,
+    private router: Router
+  ) { }
 
   ngOnInit(): void {
   }
+
+  // handleFileInput(files: FileList) {
+  //   this.image = files.item(0);
+  // }
+
+  onCreateMovie(): void {
+    const data = {
+      id: this.id,
+      title: this.title,
+      author: this.author,
+      year: this.year,
+      genre: this.genre,
+      published: this.published,
+      image: this.image
+    }
+
+    this.movieService.create(data).subscribe(response => {
+      console.log(response)
+      this.submitted = true
+    })
+    this.router.navigate(['/movies']);
+  }
+
 
 }
