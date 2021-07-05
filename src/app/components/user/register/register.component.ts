1c118fbd7e7c047bdd163bf009a98e0b5cff5393
ahadsenja <hamdanilm96@gmail.com>
2021-07-05 15:43:58 +0800

    chore: update register component

--- src/app/components/user/register/register.component.ts
+++ src/app/components/user/register/register.component.ts
@@ -1,5 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 
+import { RegisterService } from 'src/app/service/register.service';
+import { User } from 'src/app/models/user';
+import { TokenStorageService } from 'src/app/service/token-storage.service';
+
 @Component({
   selector: 'app-register',
   templateUrl: './register.component.html',
@@ -7,9 +11,40 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RegisterComponent implements OnInit {
 
-  constructor() { }
+  users: User[] = [];
+
+  id: number = 0;
+  first_name: string = '';
+  last_name: string = '';
+  email: string = '';
+  username: string = '';
+  password: string = '';
+  
+  submitted: boolean = false;
+
+  constructor(
+    private registerService: RegisterService,
+    private token: TokenStorageService
+  ) { }
 
   ngOnInit(): void {
+  }
+
+  onRegister(): void {
+    const data = {
+      id: this.id,
+      first_name: this.first_name,
+      last_name: this.last_name,
+      email: this.email,
+      username: this.username,
+      password: this.password
+    }
+
+    this.registerService.register(data).subscribe(response => {
+      this.token.saveUser(response)
+      console.log(response)
+      this.submitted = true
+    })
   }
 
 }
