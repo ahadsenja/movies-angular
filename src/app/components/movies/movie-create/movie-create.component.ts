58dbe5c3159071acca0706f45282862b4ebbb493
ahadsenja <hamdanilm96@gmail.com>
2021-07-07 10:51:57 +0800

    chore: update movie create component with formData

--- src/app/components/movies/movie-create/movie-create.component.ts
+++ src/app/components/movies/movie-create/movie-create.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { FormGroup, FormBuilder, FormControl } from '@angular/forms';
 
 import { MoviesService } from 'src/app/service/movies.service';
 import { Movie } from 'src/app/models/movie';
@@ -23,35 +24,59 @@ export class MovieCreateComponent implements OnInit {
 
   submitted = false;
 
+  createMovieForm = new FormGroup({});
+
   constructor(
     private movieService: MoviesService,
-    private router: Router
-  ) { }
+    private router: Router,
+    public formBuilder: FormBuilder
+  ) {
+    this.createMovieForm = this.formBuilder.group({
+      title: [''],
+      author: [''],
+      year: [null],
+      genre: [''],
+      published: [false],
+      image: [''],
+    })
+   }
+
+   onSelectedFile(event: any) {
+     const file = event.target.files[0];
+     this.createMovieForm.patchValue({
+       image: file
+     })
+     this.createMovieForm.get('image')?.updateValueAndValidity;
+   }
 
   ngOnInit(): void {
   }
 
-  // handleFileInput(files: FileList) {
-  //   this.image = files.item(0);
-  // }
-
   onCreateMovie(): void {
-    const data = {
-      id: this.id,
-      title: this.title,
-      author: this.author,
-      year: this.year,
-      genre: this.genre,
-      published: this.published,
-      image: this.image
-    }
+    let formData: any = new FormData();
+    formData.append('title', this.createMovieForm.get('title')?.value)
+    formData.append('author', this.createMovieForm.get('author')?.value)
+    formData.append('year', this.createMovieForm.get('year')?.value)
+    formData.append('genre', this.createMovieForm.get('genre')?.value)
+    formData.append('image', this.createMovieForm.get('image')?.value)
 
-    this.movieService.create(data).subscribe(response => {
+    this.movieService.create(formData).subscribe(response => {
       console.log(response)
       this.submitted = true
     })
+
     this.router.navigate(['/movies']);
   }
 
+  onCancel() {
+    this.id = 0;
+    this.title = '';
+    this.author = '';
+    this.year = 0;
+    this.genre = '';
+    this.published = false;
+    this.image = '';
+    this.router.navigate(['/movies'])
+  }
 
 }
