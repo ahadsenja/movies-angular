2a00c0b3b39a69c56a1cc9cf181f4b3b3f718fe4
ahadsenja <hamdanilm96@gmail.com>
2021-07-17 19:22:22 +0800

    feat: add search and detail icon

--- src/app/components/movies/movie-list/movie-list.component.ts
+++ src/app/components/movies/movie-list/movie-list.component.ts
@@ -1,12 +1,16 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 
-import { 
-  faTrashAlt, faPenAlt, faPlusSquare
-} from '@fortawesome/free-solid-svg-icons';
-
 import { Movie } from 'src/app/models/movie';
 import { MoviesService } from 'src/app/service/movies.service';
+
+import { 
+  faTrashAlt, 
+  faPenAlt, 
+  faPlusSquare, 
+  faEye, 
+  faSearch
+} from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-movie-list',
@@ -18,12 +22,16 @@ export class MovieListComponent implements OnInit {
   trashAlt = faTrashAlt
   penAlt = faPenAlt
   plusSquare = faPlusSquare
+  eye = faEye
+  search = faSearch
 
   movies: Movie[] = [];
 
+  searchText: string = '';
+
   constructor(
     private movieService: MoviesService,
-    private router: Router 
+    private router: Router
   ) { }
 
   ngOnInit(): void {
