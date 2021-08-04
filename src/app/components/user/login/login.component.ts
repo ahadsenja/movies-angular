967841f61ba2a1179720bca57cd80dd061a2b1a1
ahadsenja <hamdanilm96@gmail.com>
2021-08-04 16:42:12 +0800

    chore: update navigated route with replaceState

--- src/app/components/user/login/login.component.ts
+++ src/app/components/user/login/login.component.ts
@@ -1,5 +1,7 @@
+import { Location } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { Observable } from 'rxjs';
 
 import { AuthService } from 'src/app/service/auth.service';
 import { TokenStorageService } from 'src/app/service/token-storage.service';
@@ -15,28 +17,37 @@ export class LoginComponent implements OnInit {
     username: null,
     password: null
   }
-  isLoggedIn = false;
+
+  isLoggedIn = true
 
   constructor(
     private auth: AuthService,
     private token: TokenStorageService,
-    private router: Router
+    private router: Router,
+    private location: Location
   ) { }
 
   ngOnInit(): void {
     console.log(this.form)
-    if(this.token.getToken()) {
-      this.isLoggedIn = true
+    if(this.token.getToken() !== null) {
+      this.isLoggedIn = true;
     }
   }
 
   onSubmit(): void {
+    this.login();
+  }
+
+  login() {
     const { username, password } = this.form;
 
-    this.auth.login(username, password).subscribe(data => {
-      this.token.saveUser(data);
-      this.router.navigate(['/dashboard']);
-      console.log('Login Success');
-    })
+    if (username !== '' && password !== '') {
+      this.auth.login(username, password).subscribe(data => {
+        this.isLoggedIn = true;
+        this.token.saveUser(data);
+        this.location.replaceState('/dashboard');
+        window.location.reload();
+      })
+    }
   }
 }
